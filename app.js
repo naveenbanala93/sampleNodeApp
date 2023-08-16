bc8e81d717b757d405e4311ef7787a7f7c070746
Naveen Kumar Banala <nbanala@innominds.com>
2023-08-16 15:21:52 +0530

    mongo config changes

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express')
 const app = express()
 const port = 4000;
 
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!, This Is A Sample Application.');
 })
@@ -11,6 +21,8 @@ app.get('/', (req, res) => {
 app.get('/welcome', (req, res) => {
   res.send('Hello World!, This Is Welcome Page.');
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
